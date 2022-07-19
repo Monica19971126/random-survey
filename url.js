ce60737d934f86567d2119d15c1f836e4a2e7b5d
Monica19971126 <109280894+Monica19971126@users.noreply.github.com>
2022-07-19 19:52:18 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,8 +8,6 @@
 
 
 const urls = [
-	'https://docs.google.com/forms/d/e/1FAIpQLSfSMC1qwfxAEyYyrfvvPvMj02cEE8XbIVSC22hXOZ7bM6O-hw/viewform?usp=sf_link',
-	'https://docs.google.com/forms/d/e/1FAIpQLSee5BEG0qdpSspJR0_gs9MXLznJtJf8FZB8sySYQFF2PXV-Jg/viewform?usp=sf_link',
 	'https://docs.google.com/forms/d/e/1FAIpQLSdOOwyCuSqFDkQZSQ1xPtndHF9bT8_8pP9DFUMNpD_Yj-kK_A/viewform?usp=sf_link',
 	'https://docs.google.com/forms/d/e/1FAIpQLSes84iLz_kMK7Of-FWYIDO28eRslBhP3n1a3cKgdyAc_AoZGg/viewform?usp=sf_link',
 	'https://docs.google.com/forms/d/e/1FAIpQLScY0AH9-rygqoAw1SNY3C1UbJCzNkT-ZWGp_S4jKmBwivfE4A/viewform?usp=sf_link'];
